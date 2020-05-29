120898611189e474e5a189a290f5f42e1fdb9985
gael-src <gaelfr85@gmail.com>
2020-05-29 13:50:54 +0200

    commit

--- src/content/content.js
+++ src/content/content.js
@@ -20,5 +20,54 @@ export const cardsContent = [
         description: "blue",
         repoLink: "https://github.com/gael-src/cards-website",
         codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/80s.jpeg",
+        title: "Hello",
+        description: "red",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/batman.jpg",
+        title: "Jo Jo Jo",
+        description: "green",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/goku.jpg",
+        title: "Aligato",
+        description: "blue",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/80s.jpeg",
+        title: "Hello",
+        description: "red",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/batman.jpg",
+        title: "Jo Jo Jo",
+        description: "green",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/goku.jpg",
+        title: "Aligato",
+        description: "blue",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
+    },
+    {
+        img: "portfolio-2.0/assets/images/80s.jpeg",
+        title: "Hello",
+        description: "red",
+        repoLink: "https://github.com/gael-src/cards-website",
+        codeLink: "https://gael-src.github.io/cards-website/"
     }
 ];
