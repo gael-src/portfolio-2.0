a89a261e077b9955e2bc8668ee0a83f253d71326
gael-src <gaelfr85@gmail.com>
2020-05-29 13:22:53 +0200

    commit

--- src/content/content.js
+++ src/content/content.js
@@ -1,21 +1,21 @@
 // CARDS CONTENT 
 export const cardsContent = [
     {
-        img: "/assets/images/80s.jpeg",
+        img: "portfolio-2.0/assets/images/80s.jpeg",
         title: "Hello",
         description: "red",
         repoLink: "https://github.com/gael-src/cards-website",
         codeLink: "https://gael-src.github.io/cards-website/"
     },
     {
-        img: "/assets/images/batman.jpg",
+        img: "portfolio-2.0/assets/images/batman.jpg",
         title: "Jo Jo Jo",
         description: "green",
         repoLink: "https://github.com/gael-src/cards-website",
         codeLink: "https://gael-src.github.io/cards-website/"
     },
     {
-        img: "/assets/images/goku.jpg",
+        img: "portfolio-2.0/assets/images/goku.jpg",
         title: "Aligato",
         description: "blue",
         repoLink: "https://github.com/gael-src/cards-website",
