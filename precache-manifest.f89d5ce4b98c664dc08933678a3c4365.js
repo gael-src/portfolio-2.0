6fa7877dcb301fac7553dbda72c7481d4493dc50
gael-src <gaelfr85@gmail.com>
2020-05-29 10:30:43 +0200

    Updates

---
+++ precache-manifest.f89d5ce4b98c664dc08933678a3c4365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9699eb3be4453789afb1f5c44caa64",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d1b54c94333750b982b2",
+    "url": "/portfolio/static/css/main.4ba591a0.chunk.css"
+  },
+  {
+    "revision": "bc263db7915cd8849805",
+    "url": "/portfolio/static/js/2.b13ebdf6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b13ebdf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b54c94333750b982b2",
+    "url": "/portfolio/static/js/main.ad1c8ab0.chunk.js"
+  },
+  {
+    "revision": "aaf548f531659a9e0f90",
+    "url": "/portfolio/static/js/runtime-main.59aed842.js"
+  }
+]);
